7465e2d383218ad22a47e7214ba2175d1d89ad06
Ola Lindgård <ola.lindgard@iths.se>
2022-04-06 14:02:06 +0200

    Startat upp Lab2 i branch Lab2 och lagt till 2 entiteter.

---
+++ src/main/java/se/iths/entity/Teacher.java
@@ -0,0 +1,22 @@
+package se.iths.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+}
